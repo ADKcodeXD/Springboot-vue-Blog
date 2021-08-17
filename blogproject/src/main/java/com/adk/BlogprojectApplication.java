9af42f88555c3257d22694d937aece21061e5086
ADKcode <790532173@qq.com>
2021-08-17 23:11:39 +0800

    提交代码

---
+++ blogproject/src/main/java/com/adk/BlogprojectApplication.java
@@ -0,0 +1,13 @@
+package com.adk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogprojectApplication.class, args);
+    }
+
+}
